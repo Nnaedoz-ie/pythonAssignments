0f41ab0735791a63a61473d11607f6379569fdc7
Nnaedoz-ie <ezebonnaedozie@gmail.com>
2024-10-30 10:54:19 +0100

    Python week 1 assignment completed successfully!

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# Asking the user for their personal details
+
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where do you live? ")
+
+print(f"Hello {name}, you are {age} years old and you currently live in {location}")
